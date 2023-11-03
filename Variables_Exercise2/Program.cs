855b9bbf18bca575be95cdbe755e6adb28dab08c
Allan Young <ayounggtr@gmail.com>
2023-11-03 12:19:42 -0400

    test

---
+++ Variables_Exercise2/Program.cs
@@ -0,0 +1,12 @@
+﻿//Declaring and Initializing Variriables
+string dogName = "Geiger";
+int dogAge = 14;
+char lastNameInitial = 'Y';
+bool isBelgianMalinois = true;
+double weight = 62.5;
+decimal noseWorkScore = 99.55m;
+
+Console.WriteLine($"My dog's name is {dogName} {lastNameInitial} ('Y' is for Young). He is {dogAge} years old.");
+Console.WriteLine($"He weighs {weight} pounds and scored a {noseWorkScore} on his nose work test.");
+Console.WriteLine($"The fact theat he's not a German Shepherd but a Belgian Malinois is {isBelgianMalinois}.");
+
